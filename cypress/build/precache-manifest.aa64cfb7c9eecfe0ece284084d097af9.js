73ff15152d574a66e7ad6de4b250e379a3f5c045
Darsi raga vyshnavi <vyshudarsi@gmail.com>
2024-06-28 11:47:37 +0530

    first commit

---
+++ cypress/build/precache-manifest.aa64cfb7c9eecfe0ece284084d097af9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c6fa8388c230f497a326e9e47060a97",
+    "url": "/vyshnavi/index.html"
+  },
+  {
+    "revision": "f20367d9a1764e2727ea",
+    "url": "/vyshnavi/static/css/2.0733074b.chunk.css"
+  },
+  {
+    "revision": "e482e58d1dffdc23a484",
+    "url": "/vyshnavi/static/css/main.161a37c0.chunk.css"
+  },
+  {
+    "revision": "f20367d9a1764e2727ea",
+    "url": "/vyshnavi/static/js/2.61df5e59.chunk.js"
+  },
+  {
+    "revision": "e482e58d1dffdc23a484",
+    "url": "/vyshnavi/static/js/main.b9e42a3e.chunk.js"
+  },
+  {
+    "revision": "95222527cae8bb78eb16",
+    "url": "/vyshnavi/static/js/runtime~main.70bae06a.js"
+  }
+]);
